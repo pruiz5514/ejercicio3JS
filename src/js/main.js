a4f61bcaa4da2da6e42b5edebe0571b372fdfde2
pruiz5514 <pablo_ruiza@hotmail.com>
2024-04-17 13:42:24 -0500

    ejercicio 13

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+const suma = 2+3;
+const resta = 2-3;
+const mult = 2*3;
+const div = 6/2;
+const modulo = 6%2;
+
+const numeroAleatorio = Math.floor(Math.random()*(10-1+1)+1);
+console.log(numeroAleatorio);
+
+const potencia = 2**4;
+const raiz = Math.sqrt(49);
+const faren = (28*1.8) + 32;
+const areaCircul = Math.PI*Math.pow(5,2)
+const perimetroCuadrado = 2*4;
+const volumenEsfera = (4/3) * Math.PI * (Math.pow(2,3));
+const areaTriangulo = (8*9)/2;
+
