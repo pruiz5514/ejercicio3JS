f88b6ee63962bb886ba346bea5aeaee55901a4f2
pruiz5514 <pablo_ruiza@hotmail.com>
2024-04-18 06:42:43 -0700

    Se acaba el taller

--- src/js/main.js
+++ src/js/main.js
@@ -310,81 +310,81 @@ console.log(contador48);
 console.log('ejercicio 73');
 console.log(200-75);
 
-// ejercicio 50
-console.log('ejercicio 50');
-console.log(6*8);
+// ejercicio 74
+console.log('ejercicio 74');
+console.log(9*11);
 
-// ejercicio 51
-console.log('ejercicio 51');
-console.log(225/15);
+// ejercicio 75
+console.log('ejercicio 75');
+console.log(300/25);
 
-// ejercicio 52
-console.log('ejercicio 52');
-console.log(20%7);
+// ejercicio 76
+console.log('ejercicio 76');
+console.log(30%9);
 
-// ejercicio 53
-console.log('ejercicio 53');
-console.log(Math.floor(Math.random()*(50+40-1)+40));
+// ejercicio 77
+console.log('ejercicio 77');
+console.log(Math.floor(Math.random()*(70+60-1)+60));
 
-// ejercicio 54
-console.log('ejercicio 54');
-console.log (3**4);
+// ejercicio 78
+console.log('ejercicio 78');
+console.log (4**5);
 
-// ejercicio 55
-console.log('ejercicio 55');
-console.log(Math.sqrt(169));
+// ejercicio 79
+console.log('ejercicio 79');
+console.log(Math.sqrt(196));
 
-// ejercicio 56
-console.log('ejercicio 56');
-console.log((80*1.8) + 32);
+// ejercicio 80
+console.log('ejercicio 80');
+console.log((120*1.8) + 32);
 
-// ejercicio 57
-console.log('ejercicio 57');
+// ejercicio 81
+console.log('ejercicio 81');
 console.log(8*12);
 
-// ejercicio 58
-console.log('ejercicio 58');
-console.log(2*Math.PI*8);
+// ejercicio 82
+console.log('ejercicio 82');
+console.log(2*Math.PI*10);
 
-// ejercicio 59
-console.log('ejercicio 59');
-console.log(5**3);
+// ejercicio 83
+console.log('ejercicio 83');
+console.log(6**3);
 
-// ejercicio 60
-console.log('ejercicio 60');
-console.log((12*16)/2);
+// ejercicio 84
+console.log('ejercicio 84');
+console.log((14*18)/2);
 
-// ejercicio 61
-console.log('ejercicio 61');
-console.log(((8*12)/2)*6);
+// ejercicio 85
+console.log('ejercicio 85');
+console.log(((10*15)/2)*8);
 
-// ejercicio 62
-console.log('ejercicio 62');
+// ejercicio 86
+console.log('ejercicio 86');
 for (let i=1; i<=10; i++){
-    console.log(i*9);
+    console.log(i*10);
 }
 
-// ejercicio 63
-console.log('ejercicio 63');
-console.log(Math.max(25,30,35));
+// ejercicio 87
+console.log('ejercicio 87');
+console.log(Math.max(35,40,45));
 
-// ejercicio 64
-console.log('ejercicio 64');
-console.log(Math.min(80,95,110));
+// ejercicio 88
+console.log('ejercicio 88');
+console.log(Math.min(90,105,120));
 
-// ejercicio 65
-console.log('ejercicio 65');
-console.log((8+10+14)/3);
+// ejercicio 89
+console.log('ejercicio 89');
+console.log((9,11,15)/3);
 
-// ejercicio 66
-console.log('ejercicio 66');
-console.log(8*7*6*5*4*3*2*1);
+// ejercicio 90
+console.log('ejercicio 90');
+console.log(9*8*7*6*5*4*3*2*1);
 
-// ejercicio 67
-console.log('ejercicio 67');
-console.log(Math.round(9.2))
+// ejercicio 91
+console.log('ejercicio 91');
+console.log(Math.round(10.8))
 
-// ejercicio 68
-console.log('ejercicio 68');
-console.log(18*2);
+// ejercicio 92
+console.log('ejercicio 92');
+console.log(21*2);
 
